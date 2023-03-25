dad6641f36985917be2d92b917531d6706cb7a34
AMEDOMEY Roméo David <davidromeoamedomey@yahoo.com>
2023-03-25 17:50:26 +0100

    feat(plugin): ajout du menu lors du clique droit sur sqlprogram dans le runtime

---
+++ SqlProgram.ui/src/sqlProgram/ui/GenerateSqlFileHandler.java
@@ -0,0 +1,26 @@
+package sqlProgram.ui;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+
+public class GenerateSqlFileHandler extends AbstractHandler implements IHandler {
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		System.out.print("It's work");
+		return null;
+	}
+
+}
